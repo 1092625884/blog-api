3f23f4e001a5aa040a0e84d7e0c88650feb2400f
Red Shanks <143081628+1092625884@users.noreply.github.com>
2023-12-18 21:24:16 +0800

    Update nblog.sql

--- nblog.sql
+++ nblog.sql
@@ -1,9 +1,6 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
--- ----------------------------
--- Table structure for about
--- ----------------------------
 DROP TABLE IF EXISTS `about`;
 CREATE TABLE `about`  (
   `id` bigint(0) NOT NULL,
